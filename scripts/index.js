8fa7c554619336f3704a3e7128f7acaf38515731
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-01-16 18:13:43 +0530

    Added Timer End Functionality

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,7 @@ var minutes = document.querySelector(".minutes");
 var seconds = document.querySelector(".seconds");
 var currSelected = 1;
 var timeLeft=0;
+var interval;
 
 pomodoroBtn.addEventListener("click", function() {
 	minutes.innerHTML = "25";
@@ -13,7 +14,8 @@ pomodoroBtn.addEventListener("click", function() {
 	pomodoroBtn.classList.add("selected");
 	shortBreakBtn.classList.remove("selected");
 	longBreakBtn.classList.remove("selected");
-	var currSelected = 1;
+	currSelected = 1;
+	clearInterval(interval);
 });
 
 shortBreakBtn.addEventListener("click", function() {
@@ -22,7 +24,8 @@ shortBreakBtn.addEventListener("click", function() {
 	pomodoroBtn.classList.remove("selected");
 	shortBreakBtn.classList.add("selected");
 	longBreakBtn.classList.remove("selected");
-	var currSelected = 2;
+	currSelected = 2;
+	clearInterval(interval);
 });
 
 longBreakBtn.addEventListener("click", function() {
@@ -31,27 +34,43 @@ longBreakBtn.addEventListener("click", function() {
 	pomodoroBtn.classList.remove("selected");
 	shortBreakBtn.classList.remove("selected");
 	longBreakBtn.classList.add("selected");
-	var currSelected = 3;
+	currSelected = 3;
+	clearInterval(interval);
 });
 
 start.addEventListener("click", function() {
+	clearInterval(interval);
 	timeLeft=0;
 	if(currSelected == 1) {
-		timeLeft = 25*60;
+		timeLeft = 25*60 - 1;
 	}
 	else if(currSelected == 2) {
-		timeLeft = 5*60;
+		timeLeft = 30 - 1;
 	}
 	else {
-		timeLeft = 15*60;
+		timeLeft = 15*60 - 1;
 	}
-	setInterval(timer,1000);
+	interval = setInterval(timer,1000);
 });
 
 function timer() {
 	if(timeLeft == 0){
-	    minutes.innerHTML = "Finished";
-	    seconds.innerHTML = "";
+	    clearInterval(interval);
+	    alert("TIMER DONE");
+	    var snd = new Audio("./resources/sound.wav");  
+    	snd.play();
+	    if(currSelected == 1) {
+			minutes.innerHTML = "25";
+			seconds.innerHTML = "00";
+		}
+		else if(currSelected == 2) {
+			minutes.innerHTML = "05";
+			seconds.innerHTML = "00";
+		}
+		else {
+			minutes.innerHTML = "15";
+			seconds.innerHTML = "00";
+		}
 	} else {
 		var min=Math.floor(timeLeft/60);
 		var sec=Math.floor(timeLeft%60);
