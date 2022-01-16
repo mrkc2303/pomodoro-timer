b25798cdb739d8dfb94672680912d8c4f191b781
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-01-16 17:28:41 +0530

    Added selected functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 var pomodoroBtn = document.querySelector(".pomodoro-btn");
 var shortBreakBtn = document.querySelector(".short-break-btn");
 var longBreakBtn = document.querySelector(".long-break-btn");
+var start = document.querySelector(".start-btn");
 var minutes = document.querySelector(".minutes");
 var seconds = document.querySelector(".seconds");
+var currSelected = 1;
+var timeLeft=0;
 
 pomodoroBtn.addEventListener("click", function() {
 	minutes.innerHTML = "25";
@@ -10,6 +13,7 @@ pomodoroBtn.addEventListener("click", function() {
 	pomodoroBtn.classList.add("selected");
 	shortBreakBtn.classList.remove("selected");
 	longBreakBtn.classList.remove("selected");
+	var currSelected = 1;
 });
 
 shortBreakBtn.addEventListener("click", function() {
@@ -18,6 +22,7 @@ shortBreakBtn.addEventListener("click", function() {
 	pomodoroBtn.classList.remove("selected");
 	shortBreakBtn.classList.add("selected");
 	longBreakBtn.classList.remove("selected");
+	var currSelected = 2;
 });
 
 longBreakBtn.addEventListener("click", function() {
@@ -26,4 +31,43 @@ longBreakBtn.addEventListener("click", function() {
 	pomodoroBtn.classList.remove("selected");
 	shortBreakBtn.classList.remove("selected");
 	longBreakBtn.classList.add("selected");
+	var currSelected = 3;
 });
+
+start.addEventListener("click", function() {
+	timeLeft=0;
+	if(currSelected == 1) {
+		timeLeft = 25*60;
+	}
+	else if(currSelected == 2) {
+		timeLeft = 5*60;
+	}
+	else {
+		timeLeft = 15*60;
+	}
+	setInterval(timer,1000);
+});
+
+function timer() {
+	if(timeLeft == 0){
+	    minutes.innerHTML = "Finished";
+	    seconds.innerHTML = "";
+	} else {
+		var min=Math.floor(timeLeft/60);
+		var sec=Math.floor(timeLeft%60);
+		if(min < 10) {
+			minutes.innerHTML = "0" + min;
+		}
+		else {
+			minutes.innerHTML = min;
+		}
+		if(sec < 10){
+			seconds.innerHTML = "0" + sec;
+		}
+		else {
+			seconds.innerHTML = sec;
+		}
+	    
+	}
+	timeLeft--;
+}
